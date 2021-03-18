87b1031b504c8cc999d8cbe027436879c077683d
Kira Tanaka <kiratanaka0521@gmail.com>
2021-03-18 19:52:53 +0900

    pie successed

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -1,77 +1,10 @@
 <script>
-import { Bar } from "vue-chartjs";
-import { mapGetters } from "vuex";
-
+import { Pie } from "vue-chartjs";
 export default {
-  extends: Bar,
-  name: "chart",
-  computed: {
-    ...mapGetters("getpost", ["studyData", "areas"])
-  },
-  data() {
-    return {
-      data: {
-        labels: this.areas,
-        datasets: [
-          {
-            label: "Bar Dataset",
-            data: [100, 200, 300, 440, 550, 350],
-            backgroundColor: [
-              "rgba(255, 99, 132, 0.2)",
-              "rgba(54, 162, 235, 0.2)",
-              "rgba(255, 206, 86, 0.2)",
-              "rgba(75, 192, 192, 0.2)",
-              "rgba(153, 102, 255, 0.2)",
-              "rgba(255, 159, 64, 0.2)"
-            ],
-            borderColor: [
-              "rgba(255, 99, 132, 1)",
-              "rgba(54, 162, 235, 1)",
-              "rgba(255, 206, 86, 1)",
-              "rgba(75, 192, 192, 1)",
-              "rgba(153, 102, 255, 1)",
-              "rgba(255, 159, 64, 1)"
-            ],
-            borderWidth: 5
-          },
-          {
-            label: "Line Dataset",
-            data: [100, 200, 30, 440, 550, 350],
-            borderColor: "#CFD8DC",
-            // ボーダー下
-            fill: false,
-            type: "line",
-            lineTension: 0.3
-          }
-        ]
-      },
-      options: {
-        scales: {
-          xAxes: [
-            {
-              scaleLabel: {
-                // x軸タイトル
-                display: true,
-                labelString: "Month",
-                fontColor: "red"
-              }
-            }
-          ],
-          yAxes: [
-            {
-              ticks: {
-                beginAtZero: true,
-                stepSize: 1000,
-                min: 0
-              }
-            }
-          ]
-        }
-      }
-    };
-  },
+  extends: Pie,
+  props: ["chartdata", "options"],
   mounted() {
-    this.renderChart(this.data, this.options);
+    this.rendarChart(data, options);
   }
 };
 </script>
---
+++ src/views/Login.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="small">
+    <pie-chart :chart-data="datacollection"></pie-chart>
+    <button @click="fillData()">Randomize</button>
+  </div>
+</template>
+
+<script>
+import PieChart from "../chart/PieChart.js";
+export default {
+  components: {
+    PieChart
+  },
+  data() {
+    return {
+      datacollection: null
+    };
+  },
+  mounted() {
+    this.fillData();
+  },
+  methods: {
+    fillData() {
+      this.datacollection = {
+        labels: ["タリーズ"],
+        datasets: [
+          {
+            label: "Data One",
+            backgroundColor: "#f87979",
+            data: [10, 5]
+          }
+        ]
+      };
+    },
+    getRandomInt() {
+      return Math.floor(Math.random() * (50 - 5 + 1)) + 5;
+    }
+  }
+};
+</script>
+
+<style scoped>
+.small {
+  max-width: 600px;
+  margin: 150px auto;
+}
+</style>
