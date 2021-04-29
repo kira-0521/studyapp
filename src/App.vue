a01af2fa04060173079479de1ac931155226a2e8
Kira Tanaka <kiratanaka0521@gmail.com>
2021-04-29 15:19:11 +0900

    homeコンポーネントに説明書を追加

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@
         </div>
       </div>
     </div>
-    <MobileMenu></MobileMenu>
+    <MobileMenu @click="changeMenuOpen"></MobileMenu>
   </div>
 </template>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,6 +6,15 @@
     <button class="user" @click="logout" v-if="login_user != null">
       ログアウト
     </button>
+    <div class="instructions">
+      <h2 class="instructions__title">アプリの使い方</h2>
+      <ul class="instructions__ul">
+        <li class="instructions__li">1. Googleログイン</li>
+        <li class="instructions__li">2. 各項目を入力</li>
+        <li class="instructions__li">3. カレンダーでデータを表示</li>
+        <li class="instructions__li">4. グラフで一番集中できる場所を確認</li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -25,4 +34,22 @@ export default {
 <style lang="scss" scoped>
 @import "../assets/styles/modules/_variables.scss";
 @import "../assets/styles/views/_home.scss";
+
+.instructions {
+  margin-top: 30px;
+  padding: 1.6em 2.3em;
+  box-shadow: inset -6px -6px 14px rgb(255 255 255 / 70%),
+    inset -6px -6px 10px rgb(255 255 255 / 50%),
+    inset 6px 6px 8px rgb(255 255 255 / 8%), inset 6px 6px 10px rgb(0 0 0 / 15%);
+  border-radius: 32px;
+
+  &__title {
+    font-size: 1.1em;
+    margin-bottom: 7px;
+    color: $cTitle;
+  }
+  &__li {
+    color: $cText;
+  }
+}
 </style>
