5af2aa573b540942466cd6ad29818c1b36efd7dd
PotassiumIodide <potassium.iodide28@gmail.com>
2021-05-03 18:27:14 +0900

    [Refactor] Header.vue内のheader__innerクラスをly_headerへ変更
    header__innerクラスはヘッダーのレイアウトを規定したものだったため、
    header__innerクラスを持ったdivタグを削除し、headerタグにheader__inner
    クラスを移動した後、名前をly_headerへ変更しました。
    また、marginでヘッダー下の余白を確保すると、ヘッダーの背景色を変更した
    際に、ヘッダー下に余白ができてしまうため、heightとpaddingの調節で確保
    するよう変更しました。

--- src/views/Header.vue
+++ src/views/Header.vue
@@ -1,59 +1,57 @@
 <template>
-  <header class="header">
-    <div class="header__inner">
-      <div class="header__title">
-        <router-link to="/" class="link home">
-          <h1 class="logo">Log</h1>
-        </router-link>
-      </div>
-      <button
-        v-if="$mq === 'mobile'"
-        class="mobile-menu-btn"
-        :class="{ isActive: menuOpen }"
-        @click="mobileEvent"
-      >
-        <span></span>
-        <span></span>
-        <span></span>
-      </button>
-
-      <nav class="header__nav" v-if="$mq === 'pc'">
-        <ul class="header__ul">
-          <li class="header__li">
-            <router-link to="/input" active-class="active" class="link input">
-              <pen-tool-icon size="1.5x" class="custom-class"></pen-tool-icon>
-            </router-link>
-          </li>
-          <li class="header__li">
-            <router-link
-              to="/userdata/0"
-              active-class="active"
-              class="link userdata"
-              exact
-            >
-              <calendar-icon size="1.5x" class="custom-class"></calendar-icon>
-            </router-link>
-          </li>
-          <li class="header__li">
-            <router-link to="/graph" active-class="active" class="link graph">
-              <pie-chart-icon size="1.5x" class="custom-class"></pie-chart-icon>
-            </router-link>
-          </li>
-          <li class="header__li" v-if="photoURL">
-            <router-link to="/" class="link logout">
-              <log-out-icon
-                @click="logout"
-                size="1.5x"
-                class="custom-class"
-              ></log-out-icon>
-            </router-link>
-          </li>
-          <li class="header__li" v-if="photoURL">
-            <img :src="photoURL" class="user-img" alt="" />
-          </li>
-        </ul>
-      </nav>
+  <header class="ly_header">
+    <div class="header__title">
+      <router-link to="/" class="link home">
+        <h1 class="logo">Log</h1>
+      </router-link>
     </div>
+    <button
+      v-if="$mq === 'mobile'"
+      class="mobile-menu-btn"
+      :class="{ isActive: menuOpen }"
+      @click="mobileEvent"
+    >
+      <span></span>
+      <span></span>
+      <span></span>
+    </button>
+
+    <nav class="header__nav" v-if="$mq === 'pc'">
+      <ul class="header__ul">
+        <li class="header__li">
+          <router-link to="/input" active-class="active" class="link input">
+            <pen-tool-icon size="1.5x" class="custom-class"></pen-tool-icon>
+          </router-link>
+        </li>
+        <li class="header__li">
+          <router-link
+            to="/userdata/0"
+            active-class="active"
+            class="link userdata"
+            exact
+          >
+            <calendar-icon size="1.5x" class="custom-class"></calendar-icon>
+          </router-link>
+        </li>
+        <li class="header__li">
+          <router-link to="/graph" active-class="active" class="link graph">
+            <pie-chart-icon size="1.5x" class="custom-class"></pie-chart-icon>
+          </router-link>
+        </li>
+        <li class="header__li" v-if="photoURL">
+          <router-link to="/" class="link logout">
+            <log-out-icon
+              @click="logout"
+              size="1.5x"
+              class="custom-class"
+            ></log-out-icon>
+          </router-link>
+        </li>
+        <li class="header__li" v-if="photoURL">
+          <img :src="photoURL" class="user-img" alt="" />
+        </li>
+      </ul>
+    </nav>
   </header>
 </template>
 
@@ -88,24 +86,22 @@ export default {
 <style lang="scss" scoped>
 @import "../assets/styles/modules/_variables.scss";
 
+.ly_header {
+  height: 120px;
+  padding: 20px 30px;
+  max-width: 1070px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  @media screen and (min-width: 480px) {
+    flex-direction: row;
+    justify-content: space-evenly;
+  }
+}
 .header {
   border: none;
   background-color: $cBg;
-
-  &__inner {
-    height: 100px;
-    padding: 0 30px;
-    max-width: 1070px;
-    margin: 0 auto 20px;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-
-    @media screen and (min-width: 480px) {
-      flex-direction: row;
-      justify-content: space-evenly;
-    }
-  }
 
   &__title {
     vertical-align: middle;
